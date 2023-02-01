a638c2a82727188416fa185a98857bc1b8c5a70e
Huntrt <huntrt.business@outlook.com>
2023-02-01 23:46:31 +0700

    Use object since scene assest cause error when build

--- Feature Develop/Assets/General/Scripts/FeatureData.cs
+++ Feature Develop/Assets/General/Scripts/FeatureData.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
-using UnityEditor;
 
 public class FeatureData : MonoBehaviour
 {
 	[TextArea(10,100)] public string description;
-	public SceneAsset featureScene;
+	public Object featureScene;
 	public GameObject indicator;
 
 	//Send data of this feature to display when click it button
