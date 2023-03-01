640bc067bfef89939cd7b9b142d417c383eac32a
Huntrt <huntrt.business@outlook.com>
2023-03-01 20:53:57 +0700

    Add edge collider support to polygon line

--- Feature Develop/Assets/My Effects/Demo/PolygonLine_Testing.cs
+++ Feature Develop/Assets/My Effects/Demo/PolygonLine_Testing.cs
@@ -5,11 +5,12 @@ public class PolygonLine_Testing : MonoBehaviour
 	[SerializeField] LineRenderer line;
 	[SerializeField] float size;
 	[SerializeField] int vertexCount;
+	[SerializeField] EdgeCollider2D edgeCollider;
 
 	void Update()
 	{
 		if(Input.GetKeyDown(KeyCode.UpArrow)) vertexCount++;
 		if(Input.GetKeyDown(KeyCode.DownArrow)) vertexCount--;
-		PolygonLine.Draw(line, vertexCount, size);
+		PolygonLine.Draw(line, vertexCount, size, edgeCollider);
 	}
 }
--- Feature Develop/Assets/My Effects/PolygonLine.cs
+++ Feature Develop/Assets/My Effects/PolygonLine.cs
@@ -1,26 +1,29 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public static class PolygonLine
 {
-    public static void Draw(LineRenderer line, int vertexCount, float size)
+    public static void Draw(LineRenderer line, int vertexCount, float size, EdgeCollider2D collider = null)
 	{
 		//The last vertex to connect
 		vertexCount++;
 		//List of all vertices vector in 2d & 3d with an additional vertex
-		Vector2[] vertices = new Vector2[vertexCount]; Vector3[] vertices3d = new Vector3[vertexCount];
+		List<Vector2> vertices2 = new List<Vector2>(); Vector3[] vertices3 = new Vector3[vertexCount];
 		//Go through all the vertices
 		for (int v = 0; v < vertexCount; v++)
 		{
 			//Convert this split vertex degree to radians
 			float radians = Mathf.Deg2Rad * (v * 360 / (vertexCount-1));
 			//Get the position for this vertex using radiant has get toward size given
-			vertices[v] = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians)) * size;
+			vertices2.Add(new Vector2(Mathf.Cos(radians), Mathf.Sin(radians)) * size);
 			//Save this vertex as 2d
-			vertices3d[v] = vertices[v];
+			vertices3[v] = vertices2[v];
 		}
 		//Set given line position count
 		line.positionCount = vertexCount;
 		//Set all line position to be vertices
-		line.SetPositions(vertices3d);
+		line.SetPositions(vertices3);
+		//Add vertex to edge collider if it exist
+		if(collider != null) collider.SetPoints(vertices2);
 	}
 }
