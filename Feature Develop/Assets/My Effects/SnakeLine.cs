eb49e9ee628925e2949d6dd75b0ecc794002de2c
Huntrt <huntrt.business@outlook.com>
2023-03-02 20:47:37 +0700

    Improve snake line naming and support initial segment

--- Feature Develop/Assets/My Effects/SnakeLine.cs
+++ Feature Develop/Assets/My Effects/SnakeLine.cs
@@ -3,76 +3,93 @@ using UnityEngine;
 
 public class SnakeLine : MonoBehaviour
 {
-	public float partCount;
-	[SerializeField] float spacing;
-	public LineRenderer line;
 	public Transform head;
-	public EdgeCollider2D[] bodyColliders;
-	public List<Vector2> bodyPart = new List<Vector2>();
-	[SerializeField] List<Vector2> partPos = new List<Vector2>();
+	public LineRenderer line;
+	public int initialSegment;
+	[SerializeField] float spacing;
+	public List<Vector2> segments = new List<Vector2>();
+	[SerializeField] List<Vector2> segmentPos = new List<Vector2>();
+	[SerializeField] EdgeCollider2D collisionCollider, triggerCollider;
+
+	void OnEnable()
+	{
+		ResetPart();
+	}
 
 	public void ResetPart()
 	{
-		//Renew all the body part
-		bodyPart = new List<Vector2>(); partPos = new List<Vector2>();
-		//Remove all line position
-		line.positionCount = 0;
-		//Clear all the body collider point
-		bodyColliders[0].SetPoints(bodyPart);
-		bodyColliders[1].SetPoints(bodyPart);
-		//Add the head's position
-		partPos.Add(head.position);
+		//Renew all the segment
+		segments = new List<Vector2>(); segmentPos = new List<Vector2>();
+		//Reset collider point
+		SetColliderPoint();
+		//Add head as the frist segment position
+		segmentPos.Add(head.position);
+		//Initilize all the segment needed
+		for (int i = 0; i < initialSegment; i++)
+		{
+			segments.Add(Vector2.zero);
+			segmentPos.Add(Vector2.zero);
+		}
+		//Set line position for each segment
+		line.positionCount = segments.Count;
 	}
 
 	void LateUpdate()
 	{
-		//Only draw body if part pos for head exist
-		if(partPos.Count > 0) DrawBody();
+		//Only draw if segment pos for head exist
+		if(segmentPos.Count > 0) DrawLine();
 	}
 
-	public void DrawBody()
+	public void DrawLine()
 	{
-		//Get distance between the head and the first part
-		float dist = Vector2.Distance((Vector2)head.position, partPos[0]);
+		//Get distance between the head and the first segment
+		float dist = Vector2.Distance((Vector2)head.position, segmentPos[0]);
 		//While distance still bigger than spacing
 		while (dist > spacing)
 		{
-			//Get the direction from the first part to head
-			Vector2 dir = ((Vector2)head.position - partPos[0]).normalized;
-			//Insert position from the first toward direction using spacing
-			partPos.Insert(0, partPos[0] + dir * spacing);
-			//Remove the old first part
-			partPos.RemoveAt(partPos.Count - 1);
-			//Get leftover distance
+			//Get the direction from the first segment to head
+			Vector2 dir = ((Vector2)head.position - segmentPos[0]).normalized;
+			//Insert an position from the first segment spacing toward direction
+			segmentPos.Insert(0, segmentPos[0] + dir * spacing);
+			//Remove the last segment position
+			segmentPos.RemoveAt(segmentPos.Count - 1);
+			//Has spacing from distance
 			dist -= spacing;
 		}
-		//Array for body part's 3d position
-		Vector3[] bodyPart3d = new Vector3[bodyPart.Count];
-		//Set the frist body part to be head's position
-		if(bodyPart.Count > 0) {bodyPart[0] = head.position; bodyPart3d[0] = head.position;}
-		//Go through all the body part
-		for (int b = 1; b < bodyPart.Count; b++)
+		//Array for segment 3d position
+		Vector3[] segments3d = new Vector3[segments.Count];
+		//Set the frist segment to be the head's position
+		if(segments.Count > 0) {segments[0] = head.position; segments3d[0] = head.position;}
+		//Go through all the segment
+		for (int b = 1; b < segments.Count; b++)
 		{
-			//Lerping this part to it previous part using progress of distance
-			bodyPart[b] = Vector2.Lerp(partPos[b], partPos[b-1], dist/spacing);
-			//Save this body part 3d position
-			bodyPart3d[b] = bodyPart[b];
+			//Lerping this segment to it previous segment using progress of distance
+			segments[b] = Vector2.Lerp(segmentPos[b], segmentPos[b-1], dist/spacing);
+			//Save this segment in 3d position
+			segments3d[b] = segments[b];
 		}
-		//Add all the part position to line
-		line.SetPositions(bodyPart3d);
-		//Set points for both trigger and collision collider
-		bodyColliders[0].SetPoints(bodyPart);
-		bodyColliders[1].SetPoints(bodyPart);
+		//Apply all the segment position to line
+		line.SetPositions(segments3d);
+		//Set collider point
+		SetColliderPoint();
 	}
 
 	public void Grow()
 	{
 		//Grow another part 
-		bodyPart.Add(partPos[partPos.Count-1]);
-		partPos.Add(partPos[partPos.Count-1]);
-		//Set line position the same as body part count
-		line.positionCount = bodyPart.Count;
-		//Draw body instantly after grow
-		DrawBody();
+		segments.Add(segmentPos[segmentPos.Count-1]);
+		segmentPos.Add(segmentPos[segmentPos.Count-1]);
+		//Set line position for each segment
+		line.positionCount = segments.Count;
+		//Draw segment instantly after grow
+		DrawLine();
+	}
+
+	void SetColliderPoint()
+	{
+		//Set collision collider point if needed
+		if(collisionCollider != null) collisionCollider.SetPoints(segments);
+		//Set trigger collider point if needed
+		if(triggerCollider != null) triggerCollider.SetPoints(segments);
 	}
 }
